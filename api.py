1cd08381f8af9cd1e2fc94fc321a6cf1e422ffd5
Ashish Jha <arj7192@Ashishs-MacBook-Pro.local>
2017-07-31 10:11:12 +0200

    31-Jul, 2017: (v) Code cleanup and modularization

--- api.py
+++ api.py
@@ -1,46 +1,71 @@
-from flask import Flask, render_template, request, redirect
-import json, ast
-import datetime
+from flask import Flask, request
+import json
 from scipy import misc
 import numpy as np
 from model_inference import retrieve_model
 
 app = Flask(__name__)
-
-"""login_manager = LoginManager()
-login_manager.init_app(app)"""
-
 app.config["DEBUG"] = True
 app.config['WTF_CSRF_ENABLED'] = True
 
+# Load the pre-trained model
 model = retrieve_model()
 
 
+# Define the upload endpoint functionality
 @app.route('/upload/', methods=['POST'])
 def upload():
-   if request.method == 'POST':
-      if len((request.files)) == 0:
-         return "No files uploaded !"
-      elif len((request.files)) > 1:
-	 return "Mutliple file uploads not allowed !"
-      else:
-      	file = request.files.values()[0]
-      if file:
-        data = misc.imread(file, flatten=True)
+	"""
+		Pre-process an uploaded image file and feed it to the machine learning model to get the probabilities, and
+		return those output probabilities tupled with the most likely class label.
+		Parameters
+		----------
+		None
+		Returns
+		-------
+		result : string
+			json-stringified tuple (a, b) where a is the most likely predicted digit (between 0 and 9) and b is the
+			ordered list of probabilities for each digit.
+	"""
+	if request.method == 'POST':
+		if len((request.files)) == 0:
+			return "No files uploaded !"
+		elif len((request.files)) > 1:
+			return "Mutliple file uploads not allowed !"
+		else:
+			file = request.files.values()[0]
+		if file:
+			data = misc.imread(file, flatten=True)
+			result = model.predict(pre_process_data(data))
+			result = list(result[0])
+			final_result = np.argmax(result)
+			result = map("{0:.5f}".format, result)
+			result = json.dumps(final_result, result)
+			return result
+	else:
+		return "Please make a POST request !"
+
+
+def pre_process_data(data):
+	"""
+        Pre-process the image data matrix into a 28X28 normalized input feature matrix for the machine learning model
+        Parameters
+        ----------
+        data: list
+        	Basically the output of the imread function applied on the image file.
+        Returns
+        -------
+        data : numpy array
+        	A pre-processed, normalized, and reshaped (into size [1, 28, 28, 1]) matrix
+    """
 	data = np.array(data)
 	if data.shape != (28, 28):
-       	    data = misc.imresize(data, [28, 28])
-	    data = 256. - data
-	else:
-	    data = data - np.min(data)
-	    data = data/np.float(np.max(data))
-	
-	result = model.predict(data.reshape([1,28,28,1]))
-	result = list(result[0])
-	final_result = np.argmax(result)
-	result = map("{0:.5f}".format, result)
-	result = (final_result, result)
-	return json.dumps(result)
+		data = misc.imresize(data, [28, 28])
+		data = 256. - data
+	data = data - np.min(data)
+	data = data / np.float(np.max(data))
+	return data.reshape([1, 28, 28, 1])
+
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8888)
+	app.run(host="0.0.0.0", port=8888)
--- model_inference.py
+++ model_inference.py
@@ -1,33 +1,26 @@
 from __future__ import division, print_function, absolute_import
 
 import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.normalization import local_response_normalization
-from tflearn.layers.estimator import regression
-
+from model_training import build_network
 
 
 def retrieve_model(model_path="mnist_model.tfl"):
-	# Building convolutional network
-	network = input_data(shape=[None, 28, 28, 1], name='input')
-	network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
-	network = max_pool_2d(network, 2)
-	network = local_response_normalization(network)
-	network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
-	network = max_pool_2d(network, 2)
-	network = local_response_normalization(network)
-	network = fully_connected(network, 128, activation='tanh')
-	network = dropout(network, 0.8)
-	network = fully_connected(network, 256, activation='tanh')
-	network = dropout(network, 0.8)
-	network = fully_connected(network, 10, activation='softmax')
-	network = regression(network, optimizer='adam', learning_rate=0.01,
-                     loss='categorical_crossentropy', name='target')
-
-	# Training
+	"""
+		Load an already trained model checkpoint, restore the model using that and return the restored model object
+        Parameters
+        ----------
+        model_path : string
+        	Path to the model.
+        Returns
+        -------
+        model : <class 'tflearn.models.dnn.DNN'>
+            The restored model object
+    """
+	# Building convolutional network skeleton
+	network = build_network()
 	model = tflearn.DNN(network, tensorboard_verbose=0)
 
-	model.load("mnist_model.tfl", weights_only=True)
+	# Loading the model checkpoint
+	model.load(model_path, weights_only=True)
 
 	return model
